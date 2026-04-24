89fb4b3c2c889e93c61169e7cf9ae51e418736eb
SigovAS <SigovAS@pay-trans.ru>
2026-04-24 23:45:37 +0300

    [UPDATE] populate SDK

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ docker run -it --rm  \
        -v .:/home/ubuntu/work/yocto \
        -w /home/ubuntu/work/yocto \
        radxa-yocto:latest \
-       bash -c "kas build  kas_image.yml"
+       bash -c "kas build  kas_image.yml 
+                kas shell kas_image.yml -c 'bitbake my-image -c populate_sdk'"
        
