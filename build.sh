814666e492cbbe529ac14e22250171840c971439
SigovAS <SigovAS@pay-trans.ru>
2026-05-13 22:43:32 +0300

    [UPDATE] added swupdate target to build

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 docker build -t radxa-yocto:latest . && \
 docker run -it --rm  \
        -v .:/home/ubuntu/work/yocto \
        -w /home/ubuntu/work/yocto \
        radxa-yocto:latest \
-       bash -c "kas build  kas_image.yml 
-                kas shell kas_image.yml -c 'bitbake my-image -c populate_sdk'"
+       bash -c "kas build  kas_image.yml"
+
        
